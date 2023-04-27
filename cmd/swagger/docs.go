9adf0f537ad5a8dee5936f1a4d0a01c363b4bc23
Artem <burenotti@gmail.com>
2023-04-27 22:27:25 +0300

    🩹 Fixed docs address

--- cmd/swagger/docs.go
+++ cmd/swagger/docs.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	_ "github.com/burenotti/rtu-it-lab-recruit/docs"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/swagger"
 )
 
 func main() {
+	var addr string
+	flag.StringVar(&addr, "addr", "0.0.0.0:80", "Documentation address")
+	flag.Parse()
 	app := fiber.New()
-	app.Get("/docs/*", swagger.HandlerDefault)
-	_ = app.Listen(":8001")
+	app.Get("/*", swagger.HandlerDefault)
+	_ = app.Listen(addr)
 }
