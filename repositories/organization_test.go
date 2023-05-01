fc063c755590e7480ea60a42ad162a69d68c66df
Artem <burenotti@gmail.com>
2023-05-01 03:55:53 +0300

    ✨ Added operations on members to organization repository

--- repositories/organization_test.go
+++ repositories/organization_test.go
@@ -146,3 +146,113 @@ func (s *OrganizationRepositoryTestSuite) TestDelete() {
 	err = r.Delete(ctx, int64(orgId))
 	assert.NoError(s.T(), err, "should normally delete organization")
 }
+
+func (s *OrganizationRepositoryTestSuite) TestAddMember() {
+	ctx := context.Background()
+	r := NewOrganizationRepository(NewDatabase(s.db))
+	org := s.createTestOrg()
+	usr := s.createTestUser()
+	c := model.OrganizationMemberCreate{
+		UserID: usr.UserID,
+		Can: model.MemberPrivileges{
+			ViewEvents:    true,
+			EditEvents:    false,
+			ManageMembers: true,
+		},
+	}
+	mem, err := r.AddMember(ctx, org.OrganizationID, &c)
+	assert.NoError(s.T(), err, "should correctly create user")
+	assert.Equal(s.T(), usr.UserID, mem.UserID)
+	assert.Equal(s.T(), true, mem.Can.ViewEvents)
+	assert.Equal(s.T(), false, mem.Can.EditEvents)
+	assert.Equal(s.T(), true, mem.Can.ManageMembers)
+
+	// Test if organization does not exist
+	mem, err = r.AddMember(ctx, -1, &c)
+	assert.Nil(s.T(), mem, "member should be nil if creation failed")
+	assert.ErrorIs(s.T(), err, ErrOrganizationNotFound)
+
+	// Test if user is already member of organization
+	mem, err = r.AddMember(ctx, org.OrganizationID, &c)
+	assert.Nil(s.T(), mem, "member should be nil if creation failed")
+	assert.ErrorIs(s.T(), err, ErrUserAlreadyMember)
+
+	// Test if user does not exist
+	c.UserID = -1
+	mem, err = r.AddMember(ctx, org.OrganizationID, &c)
+	assert.Nil(s.T(), mem, "member should be nil if creation failed")
+	assert.ErrorIs(s.T(), err, ErrUserNotFound)
+}
+
+func (s *OrganizationRepositoryTestSuite) TestListMember() {
+	ctx := context.Background()
+	r := NewOrganizationRepository(NewDatabase(s.db))
+	org := s.createTestOrg()
+	usr := s.createTestUser()
+	usr2 := s.createTestUser()
+	c1 := model.OrganizationMemberCreate{
+		UserID: usr.UserID,
+		Can: model.MemberPrivileges{
+			ViewEvents:    true,
+			EditEvents:    true,
+			ManageMembers: true,
+		},
+	}
+	c2 := model.OrganizationMemberCreate{
+		UserID: usr2.UserID,
+		Can: model.MemberPrivileges{
+			ViewEvents:    true,
+			EditEvents:    false,
+			ManageMembers: false,
+		},
+	}
+	mem1, err := r.AddMember(ctx, org.OrganizationID, &c1)
+	require.NoError(s.T(), err)
+
+	mem2, err := r.AddMember(ctx, org.OrganizationID, &c2)
+	require.NoError(s.T(), err)
+
+	expectedMembers := []model.OrganizationMember{*mem1, *mem2}
+	members, err := r.ListMembers(ctx, org.OrganizationID)
+	assert.NoError(s.T(), err, "should return without errors")
+	assert.Equal(s.T(), expectedMembers, members, "should correctly list members")
+}
+
+func (s *OrganizationRepositoryTestSuite) createTestOrg() *model.Organization {
+	addr := "Moscow"
+	email := "org@example.com"
+	phone := "71111111111"
+	name := "My organization"
+	orgId := -1
+	q := `
+		INSERT INTO organizations
+		    (name, address, contact_email, contact_phone)
+		VALUES 
+			($1, $2, $3, $4)
+		RETURNING organization_id
+	`
+	err := s.db.Get(&orgId, q, name, addr, email, phone)
+	require.NoError(s.T(), err, "should insert organization without error")
+	return &model.Organization{
+		OrganizationID: int64(orgId),
+		Name:           name,
+		Address:        &addr,
+		ContactEmail:   &email,
+		ContactPhone:   &phone,
+	}
+}
+
+func (s *OrganizationRepositoryTestSuite) createTestUser() *model.User {
+	u := model.User{
+		UserID:     0,
+		FirstName:  "John",
+		LastName:   "Doe",
+		MiddleName: "",
+		Email:      "johndoe@example.com",
+		IsActive:   true,
+	}
+	q := `INSERT INTO users (first_name, last_name, middle_name, email, is_active) VALUES ($1, $2, $3, $4, $5) RETURNING user_id`
+	err := s.db.Get(&u.UserID, q, u.FirstName, u.LastName, u.MiddleName, u.Email, u.IsActive)
+	require.NoError(s.T(), err)
+	return &u
+}
