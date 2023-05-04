05ace2f19ad7b56a5677cfb0a30815f1d45ecf77
Artem <burenotti@gmail.com>
2023-05-05 02:19:22 +0300

    ♻️ Make console delivery implement delivery interface

--- services/console_delivery.go
+++ services/console_delivery.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"github.com/burenotti/rtu-it-lab-recruit/model"
 	"github.com/sirupsen/logrus"
 )
 
@@ -9,18 +10,18 @@ type ConsoleDelivery struct {
 	Logger *logrus.Logger
 }
 
-func (c *ConsoleDelivery) SendCode(_ context.Context, email string, code string) error {
+func (c *ConsoleDelivery) SendCode(_ context.Context, user *model.User, code string) error {
 	c.Logger.
-		WithField("email", email).
+		WithField("email", user.Email).
 		WithField("code", code).
 		Infof("ConsoleDelivery new login code")
 
 	return nil
 }
 
-func (c *ConsoleDelivery) SendToken(_ context.Context, email string, token string) error {
+func (c *ConsoleDelivery) SendActivationToken(_ context.Context, user *model.User, token string) error {
 	c.Logger.
-		WithField("email", email).
+		WithField("email", user.Email).
 		WithField("token", token).
 		Infof("ConsoleDelivery new activation token")
 
